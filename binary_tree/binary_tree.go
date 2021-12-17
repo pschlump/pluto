4b79e9500bd321a4813072abdcb42405c3954bf0
Philip Schlump <pschlump@gmail.com>
2021-12-17 08:45:24 -0700

    Update

--- binary_tree/binary_tree.go
+++ binary_tree/binary_tree.go
@@ -7,7 +7,11 @@ BSD 3 Clause Licensed.
 */
 
 type Comparable interface {
-	Compare(b interface{}) int
+	// Compare will return -1 (or a value less than 0) if a.Compare(b) has a < b,
+	// 0 if the two are considered to be equal, and
+	// +1 (or a value larger than 0) if a.Compare(b) has a > b.  
+	// For int this can be implemented as "a - b"
+	Compare(b Comparable) int // Compare(b interface{}) int
 }
 
 // BinaryTree is a generic type buildt on top of a slice
