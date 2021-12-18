e7c3d76b2c508fa6779a14f6701d629204ae2a6c
Philip Schlump <pschlump@gmail.com>
2021-12-18 08:03:06 -0700

    Update

--- queue/queue.go
+++ queue/queue.go
@@ -10,40 +10,43 @@ import (
 	"errors"
 )
 
-// Stack is a generic type buildt on top of a slice
-type Stack[T any] []T
+// Queue is a generic type buildt on top of a slice
+type Queue[T any] struct {
+	data []T
+	head int
+}
 
 // IsEmpty will return true if the stack is empty
-func (ns Stack[T]) IsEmpty() bool {
-	return len(ns) == 0
+func (ns *Queue[T]) IsEmpty() bool {
+	return len((*ns).data) == 0
 }
 
 // Push will push new data of type [T any] onto the stack.
-func (ns *Stack[T]) Push(t T) {
-	*ns = append(*ns, t)
+func (ns *Queue[T]) Push(t T) {
+	(*ns).data = append((*ns).data, t)
 }
 
 // An error to indicate that the stack is empty
-var ErrEmptyStack = errors.New("Empty Stack")
+var ErrEmptyQueue = errors.New("Empty Queue")
 
 // Pop will remove the top element from the stack.  An error is returned if the stack is empty.
-func (ns *Stack[T]) Pop() error {
+func (ns *Queue[T]) Pop() error {
 	if ns.IsEmpty() {
-		return ErrEmptyStack
+		return ErrEmptyQueue
 	}
-	(*ns) = (*ns)[0:len((*ns))-1]
+	(*ns).data = (*ns).data[1:len((*ns).data)]
 	return nil
 }
 
 // Length returns the number of elements in the stack.
-func (ns Stack[T]) Length() int {
-	return len(ns)
+func (ns *Queue[T]) Length() int {
+	return len((*ns).data)
 }
 
 // Peek returns the top element of the stack or an error indicating that the stack is empty.
-func (ns *Stack[T]) Peek() (*T, error) {
+func (ns *Queue[T]) Peek() (*T, error) {
 	if !ns.IsEmpty() {
-		return &((*ns)[len(*ns)-1]), nil
+		return &((*ns).data[0]), nil
 	} 
-	return nil, ErrEmptyStack
+	return nil, ErrEmptyQueue
 }
