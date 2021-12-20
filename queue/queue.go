432cf7a29258d9e225f4b9bade6516e5d0f32b23
Philip Schlump <pschlump@gmail.com>
2021-12-20 09:35:45 -0700

    Add Enqueue, Dequeue

--- queue/queue.go
+++ queue/queue.go
@@ -4,6 +4,14 @@ package queue
 Copyright (C) Philip Schlump, 2012-2021.
 
 BSD 3 Clause Licensed.
+
+Basic operations on a Queue
+
+	Enqueue() — Inserts an element to the end of the queue (Same as "Push")
+	Dequeue() — Removes an element from the start of the queue (Same as "Peek" then "Pop")
+	IsEmpty() — Returns true if the queue is empty
+	Top() — Returns the first element of the queue (Same as "Peek")
+
 */
 
 import (
@@ -26,6 +34,12 @@ func (ns *Queue[T]) Push(t T) {
 	(*ns).data = append((*ns).data, t)
 }
 
+// Enqueue is the same as Push. Enqueue will push new data of type [T any] onto the stack.
+func (ns *Queue[T]) Enqueue(t T) {
+	(*ns).data = append((*ns).data, t)
+}
+
+
 // An error to indicate that the stack is empty
 var ErrEmptyQueue = errors.New("Empty Queue")
 
@@ -34,7 +48,8 @@ func (ns *Queue[T]) Pop() error {
 	if ns.IsEmpty() {
 		return ErrEmptyQueue
 	}
-	(*ns).data = (*ns).data[1:len((*ns).data)]
+	// (*ns).data = (*ns).data[1:len((*ns).data)]
+	(*ns).data = (*ns).data[1:]
 	return nil
 }
 
@@ -49,4 +64,16 @@ func (ns *Queue[T]) Peek() (*T, error) {
 		return &((*ns).data[0]), nil
 	} 
 	return nil, ErrEmptyQueue
+}
+
+// Dequeue remove and return an element from the queue (if there is one), else return an error.
+func (ns *Queue[T]) Dequeue() (rv *T, err error) {
+	if ns.IsEmpty() {
+		err = ErrEmptyQueue
+		return
+	}
+	rv = &((*ns).data[0])
+	// (*ns).data = (*ns).data[1:len((*ns).data)]
+	(*ns).data = (*ns).data[1:]
+	return 
 }
