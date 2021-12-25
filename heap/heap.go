6902f4366f8f82c6d45524ba5d173877bf42d52c
Philip Schlump <pschlump@gmail.com>
2021-12-25 05:41:40 -0700

    Update

--- heap/heap.go
+++ heap/heap.go
@@ -1,3 +1,9 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Copyright (C) 2021 Philip Schlump. All rights reserved.
+
 package heap
 
 // xyzzy - TODO - how to append an array of T
