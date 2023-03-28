62167fef4656b063d80e429c6a70aebba86d9758
Philip Schlump <pschlump@gmail.com>
2023-03-27 19:43:58 -0600

    Not working

---
+++ stack_sll_ts/stack.go
@@ -0,0 +1,73 @@
+package stack
+
+/*
+Copyright (C) Philip Schlump, 2012-2021.
+
+BSD 3 Clause Licensed.
+*/
+
+import (
+	"errors"
+
+	"github.com/pschlump/pluto/comparable"
+	"github.com/pschlump/pluto/sllts"
+)
+
+/*
+Basic operations on a stack:
+
+*	Push — Inserts an element at the top
+*	Pop - will remove the top element from the stack.  An error is returned if the stack is empty.
+*	IsEmpty — Returns true if the stack is empty
+*	Peek — Returns the top element without removing from the stack
+
+*/
+
+// Stack is a generic type buildt on top of a slice
+type Stack[T comparable.Equality] struct {
+	data sllts.Sll[T]
+}
+
+// IsEmpty will return true if the stack is empty
+func (ns *Stack[T]) IsEmpty() bool {
+	return ns.data.IsEmpty()
+}
+
+// Push will push new data of type [T any] onto the stack.
+func (ns *Stack[T]) Push(t *T) {
+	ns.data.Push(t)
+}
+
+// An error to indicate that the stack is empty
+var ErrEmptyStack = errors.New("Empty Stack")
+
+// Pop will remove the top element from the stack.  An error is returned if the stack is empty.
+func (ns *Stack[T]) Pop() (rv *T, err error) {
+	if ns.IsEmpty() {
+		err = ErrEmptyStack
+		return
+	}
+	return ns.data.Pop()
+}
+
+// Length returns the number of elements in the stack.
+func (ns *Stack[T]) Length() int {
+	return ns.data.Length()
+}
+
+// Peek returns the top element of the stack or an error indicating that the stack is empty.
+// Some times this is refered to a 'Top'
+func (ns *Stack[T]) Peek() (*T, error) {
+	if !ns.IsEmpty() {
+		// return &((*ns)[len(*ns)-1]), nil
+		return ns.data.Peek()
+	}
+	return nil, ErrEmptyStack
+}
+
+// Truncate removes all data from the list.
+func (ns *Stack[T]) Truncate() {
+	ns.data.Truncate()
+}
+
+/* vim: set noai ts=4 sw=4: */
---
+++ stack_sll_ts/stack_test.go
@@ -0,0 +1,93 @@
+package stack
+
+/*
+Copyright (C) Philip Schlump, 2012-2021.
+
+BSD 3 Clause Licensed.
+*/
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/pschlump/pluto/comparable"
+)
+
+type TestDemo struct {
+	S string
+}
+
+var _ comparable.Equality = (*TestDemo)(nil)
+
+func (aa TestDemo) IsEqual(x comparable.Equality) bool {
+	if bb, ok := x.(TestDemo); ok {
+		if aa.S == bb.S {
+			return true
+		}
+		return false
+	} else if bb, ok := x.(*TestDemo); ok {
+		if aa.S == bb.S {
+			return true
+		}
+		return false
+	} else {
+		panic(fmt.Sprintf("Passed invalid type %T to a Compare function.", x))
+	}
+	// return false
+}
+
+func TestStack(t *testing.T) {
+	var Stk1 Stack[TestDemo]
+
+	if !Stk1.IsEmpty() {
+		t.Errorf("Expected empty stack after decleration, failed to get one.")
+	}
+
+	Stk1.Push(&TestDemo{S: "hi"})
+	Stk1.Push(&TestDemo{S: "there"})
+
+	if Stk1.IsEmpty() {
+		t.Errorf("Expected non-empty stack after 1st push, failed to get one.")
+	}
+
+	x, err := Stk1.Pop()
+	if err != nil {
+		t.Errorf("Unexpectd empty stack error after 1 pop")
+	}
+	if x.S != "there" {
+		t.Errorf("Unexpectd value")
+	}
+	x, _ = Stk1.Pop()
+	if x.S != "hi" {
+		t.Errorf("Unexpectd value")
+	}
+	x, err = Stk1.Pop()
+	if err == nil {
+		t.Errorf("Unexpectd lack of error after pop on empty stack")
+	}
+
+	Stk1.Push(&TestDemo{S: "hi"})
+	Stk1.Truncate()
+	if !Stk1.IsEmpty() {
+		t.Errorf("Expected empty stack after decleration, failed to get one.")
+	}
+
+	Stk1.Push(&TestDemo{S: "hi2"})
+	Stk1.Push(&TestDemo{S: "hi3"})
+
+	got := Stk1.Length()
+	expect := 2
+	if got != expect {
+		t.Errorf("Expected length of %d got %d", expect, got)
+	}
+
+	ss, err := Stk1.Peek()
+	if err != nil {
+		t.Errorf("Unexpectd error on non-empty stack")
+	}
+	if ss.S != "hi3" {
+		t.Errorf("Expected %s got %s", "hi3", ss.S)
+	}
+}
+
+/* vim: set noai ts=4 sw=4: */
